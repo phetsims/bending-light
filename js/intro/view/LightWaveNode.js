3f408a3639a173d5fb0da801c3188c78eb1dff17
chandrashekarBemagoni <chandrashekar.bemagoni@gmail.com>
2015-02-18 19:25:44 +0530

    Minor code refactor.

--- js/intro/view/LightWaveNode.js
+++ js/intro/view/LightWaveNode.js
@@ -10,67 +10,58 @@ define( function( require ) {
   // modules
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
-  //var Color = require( 'SCENERY/util/Color' );
+  var Color = require( 'SCENERY/util/Color' );
   var Path = require( 'SCENERY/nodes/Path' );
+  var LinearGradient = require( 'SCENERY/util/LinearGradient' );
 
   /**
    *
-   * @param transform
+   * @param modelViewTransform
    * @param lightRay
    * @constructor
    */
 
-  function LightWaveNode( transform, lightRay ) {
+  function LightWaveNode( modelViewTransform, lightRay ) {
     Node.call( this );
-    this.debug = false;
+
+    //Set the path based on the light ray shape
     //PPath that shows the oscillating wave view for the LightRay
-    /*  this.addChild( new Path( this.createPaint( transform, lightRay ) ).withAnonymousClassBody( {
-     initializer: function() {
-     //Set the path based on the light ray shape
-     setPathTo( transform.modelToView( lightRay.getWaveShape() ) );
-     //Update the gradient paint when time passes
-     lightRay.addStepListener( new VoidFunction0().withAnonymousClassBody( {
-     apply: function() {
-     setPaint( createPaint( transform, lightRay ) );
-     }
-     } ) );
-     }
-     } ) );*/
-    var wavePath = new Path( transform.modelToViewShape( lightRay.getWaveShape() ), {
-      stroke: 'red', fill: 'blue'
+    this.wavePath = new Path( modelViewTransform.modelToViewShape( lightRay.getWaveShape() ), {
+      stroke: 'red', fill: 'blue', lineWidth: modelViewTransform.modelToViewDeltaX( lightRay.getWaveWidth() )
     } );
-    this.addChild( wavePath );
+    this.addChild( this.wavePath );
     //Don't intercept mouse events
     this.setPickable( false );
   }
 
   return inherit( Node, LightWaveNode, {
 
-    createPaint: function( transform, lightRay ) {
-      /* var viewWavelength = transform.modelToViewDeltaX( lightRay.getWavelength() );
-       var directionVector = transform.modelToViewDelta( lightRay.toVector2D() ).normalized();
-       //var waveVector = directionVector.times( viewWavelength );
+    /**
+     * Update the gradient paint when time passes
+     * @param modelViewTransform
+     * @param lightRay
+     */
+    createPaint: function( modelViewTransform, lightRay ) {
+
+      /*    var viewWavelength = modelViewTransform.modelToViewDeltaX( lightRay.getWavelength() );
+       var directionVector = modelViewTransform.modelToViewDelta( lightRay.toVector2D() ).normalized();
+       // var waveVector = directionVector.times( viewWavelength );
+
        //Choose the color of the peaks
        var color = lightRay.getColor();
-       var red;
-       var black;
-       if ( this.debug ) {
-       //For debugging the phase of the transmitted wave
-       red = new Color( 1, 0, 0, 0.5 );
-       black = new Color( 0, 0, 0, 0.5 );
-       }
-       else {
-       red = new Color( color.getRed() / 255, color.getGreen() / 255, color.getBlue() / 255, Math.sqrt( lightRay.getPowerFraction() ) );
-       black = new Color( 0, 0, 0, Math.sqrt( lightRay.getPowerFraction() ) );
-       }
+       var red = new Color( color.getRed() / 255, color.getGreen() / 255, color.getBlue() / 255, Math.sqrt( lightRay.getPowerFraction() ) );
+       var black = new Color( 0, 0, 0, Math.sqrt( lightRay.getPowerFraction() ) );
+
        //Has to match perfectly with the model so that sensor readings match up
        var totalPhaseOffsetInNumberOfWavelengths = lightRay.getPhaseOffset() / 2 / Math.PI;
-       var phaseOffset = directionVector.times( transform.modelToViewDeltaX( totalPhaseOffsetInNumberOfWavelengths * lightRay.getWavelength() ) );
+       var phaseOffset = directionVector.times( modelViewTransform.modelToViewDeltaX(
+       totalPhaseOffsetInNumberOfWavelengths * lightRay.getWavelength() ) );
        //the rightmost term ensures that phase doesn't depend on angle of the beam.
-       var x0 = (phaseOffset.getX() + transform.modelToViewX( lightRay.tail.getX() ));
-       var y0 = (phaseOffset.getY() + transform.modelToViewY( lightRay.tail.getY() ));
+       //var x0 = (phaseOffset.getX() + modelViewTransform.modelToViewX( lightRay.tail.getX() ));
+       //var y0 = (phaseOffset.y + modelViewTransform.modelToViewY( lightRay.tail.y) );
        // return new GradientPaint( x0, y0, red, x0 + waveVector.getX() / 2, y0 + waveVector.getY() / 2, black, true );
-       */
+       return new LinearGradient( 0, 0, 0, 4 ).addColorStop( 0, 'red',
+       0.5, 'black' );*/
     }
   } );
 } );
