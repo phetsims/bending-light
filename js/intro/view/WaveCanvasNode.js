fc0ebc633ab7615eb37e78ea2660fa60d6db9588
chandrashekarBemagoni <chandrashekar.bemagoni@gmail.com>
2015-03-27 18:33:18 +0530

    Updated the wave canvas particle shape and enable wave transformation on step.

--- js/intro/view/WaveCanvasNode.js
+++ js/intro/view/WaveCanvasNode.js
@@ -42,7 +42,6 @@ define( function( require ) {
       for ( var i = 0; i < this.waveParticles.length; i++ ) {
         particle = this.waveParticles.get( i );
         particleWidth = this.modelViewTransform.modelToViewDeltaX( particle.width );
-        context.beginPath();
         x = this.modelViewTransform.modelToViewX( particle.position.x );
         y = this.modelViewTransform.modelToViewY( particle.position.y );
         angle = particle.angle;
@@ -51,16 +50,18 @@ define( function( require ) {
         point2X = x - (particleWidth * Math.sin( angle ) / 2);
         point2Y = y - (particleWidth * Math.cos( angle ) / 2);
         var lineWidth = this.modelViewTransform.modelToViewDeltaX( particle.height );
-        context.lineWidth = lineWidth;
-        context.moveTo( point1X, point1Y );
-        context.lineTo( point2X, point2Y );
-        var gradient = context.createLinearGradient( x, y, x + lineWidth * Math.cos( angle ), y - lineWidth * Math.sin( angle ) );
+        var gradient = context.createLinearGradient( x, y, x - lineWidth * Math.cos( angle ), y + lineWidth * Math.sin( angle ) );
         gradient.addColorStop( 0, particle.color );
         gradient.addColorStop( 0.5, particle.particleGradientColor );
         gradient.addColorStop( 1, particle.color );
-        //context.createLinearGradient( point1X, point1Y, point2X, point2Y ).addColorStop( 0, "red" ).addColorStop( 0, "green" );
-        context.strokeStyle = gradient;//particle.color;
-        context.stroke();
+        context.beginPath();
+        context.moveTo( point2X, point2Y );
+        context.lineTo( point1X, point1Y );
+        context.lineTo( point1X - lineWidth * Math.cos( angle ), point1Y + lineWidth * Math.sin( angle ) );
+        context.lineTo( point2X - lineWidth * Math.cos( angle ), point2Y + lineWidth * Math.sin( angle ) );
+        context.closePath();
+        context.fillStyle = gradient;
+        context.fill();
       }
     },
 
