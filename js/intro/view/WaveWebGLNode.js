31b2e9a1e74f09b6d56798796ebfbb205b5cc03f
chandrashekarBemagoni <chandrashekar.bemagoni@gmail.com>
2015-04-28 13:00:51 +0530

    Fixed wave color issue , reduced Vector2 allocation and  refactored the code.

--- js/intro/view/WaveWebGLNode.js
+++ js/intro/view/WaveWebGLNode.js
@@ -81,20 +81,15 @@ define( function( require ) {
         'float waveLength = float(vWaveLength);',
         'float distance = dot(vec2(cos(vAngle),sin(vAngle)), vec2(x1-vTail.x,y1-vTail.y));',
         'float positionDiff = abs(distance)>vPhase?distance - vPhase:distance- vPhase +vWaveLength;',
-        'for(int i=0;i<100;i++){',
         'if(abs(positionDiff)>waveLength){',
-        'positionDiff = positionDiff>0.0?positionDiff -waveLength:positionDiff +waveLength;',
-        '}',
-        'else{',
-        'break;',
-        '}',
+        'positionDiff =mod(positionDiff, vWaveLength);',
         '}',
         'if(abs(positionDiff)<waveLength/2.0){',
-        'gl_FragColor = vec4( vColor*(abs(positionDiff)/(waveLength/2.0)) + vec3(0,0,0)*(1.0-abs(positionDiff)/(waveLength/2.0)),vPowerFraction ) ;',
+        'gl_FragColor = vec4( ( vColor*(abs(positionDiff)/(waveLength/2.0)) + vec3(0,0,0)*(1.0-abs(positionDiff)/(waveLength/2.0)) )*vPowerFraction,vPowerFraction ) ;',
         '}',
         'else{',
         'positionDiff = positionDiff>0.0?positionDiff -waveLength/2.0:positionDiff +waveLength/2.0;',
-        'gl_FragColor = vec4( vec3(0,0,0)*(1.0-abs(positionDiff)/(waveLength/2.0)) + vColor*(((waveLength/2.0)-abs(positionDiff))/(waveLength/2.0)),vPowerFraction ) ;',
+        'gl_FragColor = vec4(( vec3(0,0,0)*(1.0-abs(positionDiff)/(waveLength/2.0)) + vColor*(((waveLength/2.0)-abs(positionDiff))/(waveLength/2.0)))*vPowerFraction,vPowerFraction ) ;',
         '}',
         '}'
       ].join( '\n' );
@@ -119,18 +114,22 @@ define( function( require ) {
       for ( var i = this.rays.length - 1; i >= 0; i-- ) {
         var lightRay = this.rays.get( i );
 
-        var point1 = this.modelViewTransform.modelToViewPosition( lightRay.getWaveShape().subpaths[ 0 ].points[ 0 ] );
-        var point2 = this.modelViewTransform.modelToViewPosition( lightRay.getWaveShape().subpaths[ 0 ].points[ 1 ] );
-        var point3 = this.modelViewTransform.modelToViewPosition( lightRay.getWaveShape().subpaths[ 0 ].points[ 3 ] );
-        var point4 = this.modelViewTransform.modelToViewPosition( lightRay.getWaveShape().subpaths[ 0 ].points[ 2 ] );
+        var point1X = this.modelViewTransform.modelToViewX( lightRay.getWaveShape().subpaths[ 0 ].points[ 0 ].x );
+        var point1Y = this.modelViewTransform.modelToViewY( lightRay.getWaveShape().subpaths[ 0 ].points[ 0 ].y );
+        var point2X = this.modelViewTransform.modelToViewX( lightRay.getWaveShape().subpaths[ 0 ].points[ 1 ].x );
+        var point2Y = this.modelViewTransform.modelToViewY( lightRay.getWaveShape().subpaths[ 0 ].points[ 1 ].y );
+        var point3X = this.modelViewTransform.modelToViewX( lightRay.getWaveShape().subpaths[ 0 ].points[ 3 ].x );
+        var point3Y = this.modelViewTransform.modelToViewY( lightRay.getWaveShape().subpaths[ 0 ].points[ 3 ].y );
+        var point4X = this.modelViewTransform.modelToViewX( lightRay.getWaveShape().subpaths[ 0 ].points[ 2 ].x );
+        var point4Y = this.modelViewTransform.modelToViewY( lightRay.getWaveShape().subpaths[ 0 ].points[ 2 ].y );
 
         // elements
-        elements.push( point1.x, point1.y, 1 );
-        elements.push( point2.x, point2.y, 1 );
-        elements.push( point3.x, point3.y, 1 );
-        elements.push( point3.x, point3.y, 1 );
-        elements.push( point2.x, point2.y, 1 );
-        elements.push( point4.x, point4.y, 1 );
+        elements.push( point1X, point1Y, 1 );
+        elements.push( point2X, point2Y, 1 );
+        elements.push( point3X, point3Y, 1 );
+        elements.push( point3X, point3Y, 1 );
+        elements.push( point2X, point2Y, 1 );
+        elements.push( point4X, point4Y, 1 );
         var numberOfVertexPoints = 6;
 
         // light ray power fraction
@@ -142,22 +141,23 @@ define( function( require ) {
         var blue = lightRay.getColor().b / 255;
 
         // tail position in view co-ordinates
-        var tailView = this.modelViewTransform.modelToViewPosition( lightRay.tail );
+        var tailViewX = this.modelViewTransform.modelToViewX( lightRay.tail.x );
+        var tailViewY = this.modelViewTransform.modelToViewY( lightRay.tail.y );
 
         // light ray  angle
         var lightRayAngle = lightRay.getAngle();
 
         //  phase
         var totalPhaseOffsetInNumberOfWavelengths = lightRay.getPhaseOffset() / 2 / Math.PI;
-        var phaseOffset = lightRay.getUnitVector().times( this.modelViewTransform.modelToViewDeltaX( totalPhaseOffsetInNumberOfWavelengths * lightRay.getWavelength() ) );
-        var phaseDiff = phaseOffset.magnitude() % this.modelViewTransform.modelToViewDeltaX( lightRay.wavelength );
+        var phaseDiff = this.modelViewTransform.modelToViewDeltaX( (totalPhaseOffsetInNumberOfWavelengths % 1) * lightRay.wavelength );
+
         if ( totalPhaseOffsetInNumberOfWavelengths < 0 ) {
           phaseDiff = this.modelViewTransform.modelToViewDeltaX( lightRay.wavelength ) - phaseDiff;
         }
         for ( var k = 0; k < numberOfVertexPoints; k++ ) {
           powerFractionArray.push( lightRayPowerFraction );
           colorArray.push( red, green, blue );
-          tailPointArray.push( tailView.x, tailView.y );
+          tailPointArray.push( tailViewX, tailViewY );
           anglesArray.push( lightRayAngle );
           waveLengthArray.push( this.modelViewTransform.modelToViewDeltaX( lightRay.wavelength ) );
           phaseArray.push( phaseDiff );
