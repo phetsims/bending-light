6e553be4f53993f53e72473516f476c692c48065
chandrashekarBemagoni <chandrashekar.bemagoni@gmail.com>
2015-07-16 11:13:47 +0530

    Fixed format percentages and added documentation for @param . See #103, #94

--- js/prisms/model/SemiCircle.js
+++ js/prisms/model/SemiCircle.js
@@ -59,8 +59,8 @@ define( function( require ) {
 
     /**
      * @public
-     * @param {number} deltaX
-     * @param {number} deltaY
+     * @param {number} deltaX - distance in x direction to be translated
+     * @param {number} deltaY - distance in y direction to be translated
      * @returns {SemiCircle}
      */
     getTranslatedInstance: function( deltaX, deltaY ) {
