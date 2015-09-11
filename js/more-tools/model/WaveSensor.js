90ad76fe6f5ced3840a4bf4a70314e35bd06dfa8
Sam Reid <reids@colorado.edu>
2015-09-11 11:17:16 -0600

    Made WaveSensor extend PropertySet, see #198

--- js/more-tools/model/WaveSensor.js
+++ js/more-tools/model/WaveSensor.js
@@ -12,9 +12,9 @@ define( function( require ) {
   // modules
   var inherit = require( 'PHET_CORE/inherit' );
   var Vector2 = require( 'DOT/Vector2' );
-  var PropertySet = require( 'AXON/PropertySet' );
   var Property = require( 'AXON/Property' );
   var DataPoint = require( 'BENDING_LIGHT/more-tools/model/DataPoint' );
+  var PropertySet = require( 'AXON/PropertySet' );
 
   /**
    * Model for a probe, including its position and recorded data series
@@ -65,18 +65,23 @@ define( function( require ) {
     // looking location. This amount of precision is unnecessary, but these values were just sampled directly.
     this.probe1 = new Probe( -0.00001932, -0.0000052 );
     this.probe2 = new Probe( -0.0000198, -0.0000062 );
-    Property.addProperty( this, 'bodyPosition', new Vector2( -0.0000172, -0.00000605 ) );
 
-    // @public
-    // in the play area
-    Property.addProperty( this, 'enabled', false );
+    PropertySet.call( this, {
+
+      // @public
+      bodyPosition: new Vector2( -0.0000172, -0.00000605 ),
+
+      // @public
+      // in the play area
+      enabled: false
+    } );
 
     // Function for getting data from a probe at the specified point
     this.probe1Value = probe1Value;
     this.probe2Value = probe2Value;
   }
 
-  return inherit( Object, WaveSensor, {
+  return inherit( PropertySet, WaveSensor, {
 
     /**
      * @public
@@ -134,8 +139,7 @@ define( function( require ) {
      * @public
      */
     reset: function() {
-      this.bodyPositionProperty.reset();
-      this.enabledProperty.reset();
+      PropertySet.prototype.reset.call( this );
       this.probe1.reset();
       this.probe2.reset();
     }
