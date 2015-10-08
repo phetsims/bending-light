5b53d35a902b478a21a6926aa26fd685d3b5dfcc
Sam Reid <reids@colorado.edu>
2015-10-08 10:59:01 -0600

    Fixed name typo, see #255

--- js/bending-light-main.js
+++ js/bending-light-main.js
@@ -24,7 +24,7 @@ define( function( require ) {
       leadDesign: 'Amy Rouinfar (HTML5),  Noah Podolefsky (Java)',
       softwareDevelopment: 'Sam Reid',
       team: 'Trish Loeblein, Ariel Paul, Kathy Perkins',
-      qualityAssurance: 'Steele Dalton, Elise Morgan, Oliver Orejola, Arnab Purkayastha, Brian Yoelin',
+      qualityAssurance: 'Steele Dalton, Elise Morgan, Oliver Orejola, Arnab Purkayastha, Bryan Yoelin',
       thanks: 'Thanks to Actual Concepts for working with the PhET development team\nto convert this simulation to HTML5.'
     }
   };
