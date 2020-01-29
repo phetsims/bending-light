ad78b2ab66b48cb1b20f27ea40f72783ede9b9f1
phet-dev <phetadmin@gmail.com>
2020-01-29 03:06:48 -0700

    update copyright dates from daily grunt work

--- js/more-tools/view/WaveSensorNode.js
+++ js/more-tools/view/WaveSensorNode.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2019, University of Colorado Boulder
+// Copyright 2015-2020, University of Colorado Boulder
 
 /**
  * View for the wave sensor, which shows 2 sensor probes and a chart area (the body)
