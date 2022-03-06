14f467cd25d66882b6a38315e868ba7084c6380a
phet-dev <phetadmin@gmail.com>
2022-03-06 03:18:50 -0700

    update copyright dates from daily grunt work

--- js/common/view/MediumControlPanel.ts
+++ js/common/view/MediumControlPanel.ts
@@ -1,4 +1,4 @@
-// Copyright 2015-2021, University of Colorado Boulder
+// Copyright 2015-2022, University of Colorado Boulder
 
 /**
  * Controls for changing and viewing the medium type, including its current index of refraction
