a51901e82fba8411b4d038f076a314711e29214e
phet-dev <phetadmin@gmail.com>
2022-03-24 03:11:13 -0600

    update copyright dates from daily grunt work

--- js/common/view/LaserNode.ts
+++ js/common/view/LaserNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2015-2021, University of Colorado Boulder
+// Copyright 2015-2022, University of Colorado Boulder
 
 /**
  * Node for drawing the laser itself, including an on/off button and ability to rotate/translate.
