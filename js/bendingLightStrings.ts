5822e6bd5569d157ceff2c5add27b8957b879587
Sam Reid <reids@colorado.edu>
2022-05-07 09:35:42 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/bendingLightStrings.ts
+++ js/bendingLightStrings.ts
@@ -9,39 +9,39 @@ import bendingLight from './bendingLight.js';
 
 type StringsType = {
   'bending-light': {
-    'title': string
-  },
-  'intro': string,
-  'air': string,
-  'slowMotion': string,
-  'normalSpeed': string,
-  'normalLine': string,
-  'angles': string,
-  'water': string,
-  'glass': string,
-  'custom': string,
-  'prisms': string,
-  'mysteryA': string,
-  'mysteryB': string,
-  'material': string,
-  'units_nm': string,
-  'indexOfRefraction': string,
-  'unknown': string,
-  'objects': string,
-  'environment': string,
-  'intensity': string,
-  'ray': string,
-  'wave': string,
-  'diamond': string,
-  'time': string,
-  'protractor': string,
-  'moreTools': string,
-  'reflections': string,
-  'speed': string,
-  'wavelengthPattern': string,
-  'unknownVelocity': string,
-  'velocityPattern': string,
-  'pattern_0value_percent': string
+    'title': string;
+  };
+  'intro': string;
+  'air': string;
+  'slowMotion': string;
+  'normalSpeed': string;
+  'normalLine': string;
+  'angles': string;
+  'water': string;
+  'glass': string;
+  'custom': string;
+  'prisms': string;
+  'mysteryA': string;
+  'mysteryB': string;
+  'material': string;
+  'units_nm': string;
+  'indexOfRefraction': string;
+  'unknown': string;
+  'objects': string;
+  'environment': string;
+  'intensity': string;
+  'ray': string;
+  'wave': string;
+  'diamond': string;
+  'time': string;
+  'protractor': string;
+  'moreTools': string;
+  'reflections': string;
+  'speed': string;
+  'wavelengthPattern': string;
+  'unknownVelocity': string;
+  'velocityPattern': string;
+  'pattern_0value_percent': string;
 };
 
 const bendingLightStrings = getStringModule( 'BENDING_LIGHT' ) as StringsType;
