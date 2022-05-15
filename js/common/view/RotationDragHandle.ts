41aa91f85253f96a8881cc7aa23f0269f66fe404
phet-dev <phetadmin@gmail.com>
2022-05-15 03:16:27 -0600

    update copyright dates from daily grunt work

--- js/common/view/RotationDragHandle.ts
+++ js/common/view/RotationDragHandle.ts
@@ -1,4 +1,4 @@
-// Copyright 2015-2021, University of Colorado Boulder
+// Copyright 2015-2022, University of Colorado Boulder
 
 /**
  * Graphic that depicts how the laser may be moved (in one direction).
