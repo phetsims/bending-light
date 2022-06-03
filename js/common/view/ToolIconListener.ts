ad3a5be45127bcafa1fd02e56aadad3be6d686d2
phet-dev <phetadmin@gmail.com>
2022-06-03 03:18:49 -0600

    update copyright dates from daily grunt work

--- js/common/view/ToolIconListener.ts
+++ js/common/view/ToolIconListener.ts
@@ -1,4 +1,4 @@
-// Copyright 2015-2021, University of Colorado Boulder
+// Copyright 2015-2022, University of Colorado Boulder
 
 /**
  * Listener for tool icons (in the toolbox) used for creating a node and forwarding events to its listener.
--- js/common/view/WavelengthControl.ts
+++ js/common/view/WavelengthControl.ts
@@ -1,4 +1,4 @@
-// Copyright 2015-2021, University of Colorado Boulder
+// Copyright 2015-2022, University of Colorado Boulder
 
 /**
  * Control that shows a spectrum and lets the user choose the color for monochromatic light.
