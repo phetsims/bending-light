af1b68436a027304d304133794e8c2a2605a12b7
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:39 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/bendingLightStrings.ts
+++ js/bendingLightStrings.ts
@@ -5,43 +5,76 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import bendingLight from './bendingLight.js';
 
 type StringsType = {
   'bending-light': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'intro': string;
+  'introProperty': TReadOnlyProperty<string>;
   'air': string;
+  'airProperty': TReadOnlyProperty<string>;
   'slowMotion': string;
+  'slowMotionProperty': TReadOnlyProperty<string>;
   'normalSpeed': string;
+  'normalSpeedProperty': TReadOnlyProperty<string>;
   'normalLine': string;
+  'normalLineProperty': TReadOnlyProperty<string>;
   'angles': string;
+  'anglesProperty': TReadOnlyProperty<string>;
   'water': string;
+  'waterProperty': TReadOnlyProperty<string>;
   'glass': string;
+  'glassProperty': TReadOnlyProperty<string>;
   'custom': string;
+  'customProperty': TReadOnlyProperty<string>;
   'prisms': string;
+  'prismsProperty': TReadOnlyProperty<string>;
   'mysteryA': string;
+  'mysteryAProperty': TReadOnlyProperty<string>;
   'mysteryB': string;
+  'mysteryBProperty': TReadOnlyProperty<string>;
   'material': string;
+  'materialProperty': TReadOnlyProperty<string>;
   'units_nm': string;
+  'units_nmProperty': TReadOnlyProperty<string>;
   'indexOfRefraction': string;
+  'indexOfRefractionProperty': TReadOnlyProperty<string>;
   'unknown': string;
+  'unknownProperty': TReadOnlyProperty<string>;
   'objects': string;
+  'objectsProperty': TReadOnlyProperty<string>;
   'environment': string;
+  'environmentProperty': TReadOnlyProperty<string>;
   'intensity': string;
+  'intensityProperty': TReadOnlyProperty<string>;
   'ray': string;
+  'rayProperty': TReadOnlyProperty<string>;
   'wave': string;
+  'waveProperty': TReadOnlyProperty<string>;
   'diamond': string;
+  'diamondProperty': TReadOnlyProperty<string>;
   'time': string;
+  'timeProperty': TReadOnlyProperty<string>;
   'protractor': string;
+  'protractorProperty': TReadOnlyProperty<string>;
   'moreTools': string;
+  'moreToolsProperty': TReadOnlyProperty<string>;
   'reflections': string;
+  'reflectionsProperty': TReadOnlyProperty<string>;
   'speed': string;
+  'speedProperty': TReadOnlyProperty<string>;
   'wavelengthPattern': string;
+  'wavelengthPatternProperty': TReadOnlyProperty<string>;
   'unknownVelocity': string;
+  'unknownVelocityProperty': TReadOnlyProperty<string>;
   'velocityPattern': string;
+  'velocityPatternProperty': TReadOnlyProperty<string>;
   'pattern_0value_percent': string;
+  'pattern_0value_percentProperty': TReadOnlyProperty<string>;
 };
 
 const bendingLightStrings = getStringModule( 'BENDING_LIGHT' ) as StringsType;
