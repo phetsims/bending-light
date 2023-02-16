252e2689a1a3aa34b7be06907909b3787ad46514
phet-dev <phetadmin@gmail.com>
2023-02-16 03:59:08 -0700

    update copyright dates from daily grunt work

--- js/common/model/BendingLightModel.ts
+++ js/common/model/BendingLightModel.ts
@@ -1,4 +1,4 @@
-// Copyright 2015-2022, University of Colorado Boulder
+// Copyright 2015-2023, University of Colorado Boulder
 
 /**
  * Main model for bending light application. Rays are recomputed whenever laser parameters changed. Each ray oscillates
