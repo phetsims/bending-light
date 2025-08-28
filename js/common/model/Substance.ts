bba238c3d83e6e018ee9b4d2aae3e3d615467934
phet-dev <phetadmin@gmail.com>
2025-08-28 03:38:53 -0600

    update copyright dates from daily grunt work

--- js/common/model/Substance.ts
+++ js/common/model/Substance.ts
@@ -1,4 +1,4 @@
-// Copyright 2015-2024, University of Colorado Boulder
+// Copyright 2015-2025, University of Colorado Boulder
 
 /**
  * Immutable state for a medium, with the name and dispersion function, and flags for "mystery" and "custom".
